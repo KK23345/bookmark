77c0397f868c79ae8d886fdd13b0aa94bcb338ca
unknown <1774658209@qq.com>
2022-06-08 20:59:13 +0800

    login register

--- src/test/java/com/bookmark/DaoTest.java
+++ src/test/java/com/bookmark/DaoTest.java
@@ -1,6 +1,7 @@
 package com.bookmark;
 
 import com.bookmark.dao.UserDao;
+import com.bookmark.pojo.Book;
 import com.bookmark.pojo.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,18 +15,28 @@ public class DaoTest {
 
     @Test
     void contextLoads() {
-//        System.out.println(userDao.getByUId(2));
-//        System.out.println(userDao.getByUName("zhangsan"));
+        Book book=new Book();
+        book.setChildren("");
+        book.setFavorites(0);
+        book.setID(0);
+        book.setParentID(-1);
+        book.setUrl("");
+        book.setUid(0);
+        book.setType(0);
+        book.setIsPublic(0);
+        book.setTitle("");
+        userDao.addBook(book);
+        /**
+         User user = new User();
+         user.setName("kk");
+         user.setPassword("1111111");
+         user.setRootID(2);
+         System.out.println(userDao.addUser(user));
 
-        User user = new User();
-        user.setName("kk");
-        user.setPassword("1111111");
-        user.setRootID(2);
-        System.out.println(userDao.addUser(user));
-
-        user = userDao.getByUName("kk");
-        user.setPassword("123456");
-        System.out.println(user);
+         user = userDao.getByUName("kk");
+         user.setPassword("123456");
+         System.out.println(user);
+         ***/
         //System.out.println(userDao.updateUser(user));
 
 //        System.out.println(userDao.deleteByUId(user.getUid()));
